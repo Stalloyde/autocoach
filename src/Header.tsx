5bd027bc3891db8e0152cd92ced572dc817f40b5
Stalloyde <stalloyde93@gmail.com>
2025-01-02 13:25:01 +0800

    Fix header spacing

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,14 +3,27 @@ import { Link } from 'react-router';
 
 function Header() {
     return (
-        <div className="flex justify-between border border-slate-950 p-2">
-            <div className="p-2">Logo</div>
-            <h1 className="p-2 font-extrabold">AutoGo</h1>
-            <div className="content-center">
+        <div className="grid grid-cols-[1fr_4fr_1fr] border border-slate-950 p-2">
+            <div className="content-center p-2">Logo</div>
+            <h1 className="grid justify-center p-2 text-2xl font-extrabold">
+                AUTO-GO
+            </h1>
+            <div className="grid content-center justify-center">
                 <div className="text-xs italic">Save workouts & more..</div>
-                <div className="flex justify-center gap-10">
-                    <Link to="login">Login</Link>
-                    <Link to="signup">Sign Up</Link>
+                <div className="flex justify-center gap-2">
+                    <Link
+                        to="login"
+                        className="decoration-green-700 hover:underline"
+                    >
+                        Login
+                    </Link>{' '}
+                    /
+                    <Link
+                        to="signup"
+                        className="decoration-green-700 hover:underline"
+                    >
+                        Sign Up
+                    </Link>
                 </div>
             </div>
         </div>
