2a325d2fe7ea1fa36063977d458ede07a1489c8d
Stalloyde <stalloyde93@gmail.com>
2025-01-26 23:05:05 +0800

    Fix typo

--- src/helpers/APIurl.tsx
+++ src/helpers/APIurl.tsx
@@ -1,4 +1,4 @@
 export default function APIurl(feature: string) {
-    return `https://autocoach-api.fly.dev//${feature}`;
+    return `https://autocoach-api.fly.dev/${feature}`;
     // return `http://localhost:3000/${feature}`; //swap return statement for dev
 }
