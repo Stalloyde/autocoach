44b39517f574e8b0619c024f9298aeca256ffdbb
Stalloyde <stalloyde93@gmail.com>
2025-01-27 12:20:11 +0800

    Try to remove audio latency in mobile browser

--- src/helpers/playAudio.tsx
+++ src/helpers/playAudio.tsx
@@ -4,8 +4,11 @@ let tymAudio = new Audio('/take-your-marks-audio.mp3');
 let fiveSecondsAudio = new Audio('/five-seconds-audio.mp3');
 let workoutCompletedAudio = new Audio('/workout-completed-audio.mp3');
 
-[goAudio, tymAudio, fiveSecondsAudio, workoutCompletedAudio].forEach((audio) =>
-    audio.load()
+[goAudio, tymAudio, fiveSecondsAudio, workoutCompletedAudio].forEach(
+    (audio) => {
+        audio.preload = 'auto';
+        audio.load();
+    }
 );
 
 function playGo() {
