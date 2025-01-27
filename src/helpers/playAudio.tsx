a78779e1ee903c58a22e79c34f1578d86feb8ed0
Stalloyde <stalloyde93@gmail.com>
2025-01-27 14:09:14 +0800

    Remove Howle  -doesn't solve problem

--- src/helpers/playAudio.tsx
+++ src/helpers/playAudio.tsx
@@ -1,42 +1,22 @@
-import { Howl } from 'howler';
-
-const sounds = {
-    goAudio: new Howl({
-        src: ['/go-audio.mp3'],
-        html5: true,
-        preload: true,
-    }),
-    tymAudio: new Howl({
-        src: ['/take-your-marks-audio.mp3'],
-        html5: true,
-        preload: true,
-    }),
-    fiveSecondsAudio: new Howl({
-        src: ['/five-seconds-audio.mp3'],
-        html5: true,
-        preload: true,
-    }),
-    workoutCompletedAudio: new Howl({
-        src: ['/workout-completed-audio.mp3'],
-        html5: true,
-        preload: true,
-    }),
-};
+let goAudio = new Audio('/go-audio.mp3');
+let tymAudio = new Audio('/take-your-marks-audio.mp3');
+let fiveSecondsAudio = new Audio('/five-seconds-audio.mp3');
+let workoutCompletedAudio = new Audio('/workout-completed-audio.mp3');
 
 function playGo() {
-    sounds.goAudio.play();
+    goAudio.play();
 }
 
 function playTYM() {
-    sounds.tymAudio.play();
+    tymAudio.play();
 }
 
 function playFiveSeconds() {
-    sounds.fiveSecondsAudio.play();
+    fiveSecondsAudio.play();
 }
 
 function playWorkoutCompleted() {
-    sounds.workoutCompletedAudio.play();
+    workoutCompletedAudio.play();
 }
 
 export { playGo, playTYM, playFiveSeconds, playWorkoutCompleted };
